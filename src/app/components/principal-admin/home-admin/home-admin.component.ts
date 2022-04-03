71751bd880cfe58cea67a4c70fb1a1df10f52cf4
Palacios <bryanpalaciosg8@gmail.com>
2022-04-03 16:36:32 -0500

    Acciones en el administrador

--- src/app/components/principal-admin/home-admin/home-admin.component.ts
+++ src/app/components/principal-admin/home-admin/home-admin.component.ts
@@ -1,7 +1,10 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Mascota } from 'src/app/models/mascota';
 import { Solicitud } from 'src/app/models/solicitud';
 import { MascotaService } from 'src/app/services/mascota.service';
 import { SolicitudService } from 'src/app/services/solicitud.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home-admin',
@@ -15,6 +18,16 @@ export class HomeAdminComponent implements OnInit {
   mascotaModal:any= [];
 
   constructor( private solicitudService:SolicitudService, private mascotaService: MascotaService) { }
+  
+  objMascota:Mascota={
+    id:null,
+    nombre:'',
+    especie:'',
+    fechaNacimiento:'',
+    sexo: '',
+    estado: null,
+    imagen: '',
+  }
 
   ngOnInit(): void {
     this.loadSolicitudes();
@@ -43,4 +56,68 @@ export class HomeAdminComponent implements OnInit {
     )
   }
 
+  statusRechazado(id:string): void{
+    this.getMascotaR(id);
+    //this.updateMascota();
+  }
+  statusAprobado(id:string): void{
+    this.getMascotaA(id);
+    //this.updateMascota();
+  }
+
+  cambiarEstadoM(estado:number):void{
+    this.objMascota.id=this.mascotaModal.id,
+    this.objMascota.nombre=this.mascotaModal.nombre,
+    this.objMascota.especie=this.mascotaModal.especie,
+    this.objMascota.fechaNacimiento=this.mascotaModal.fechaNacimiento,
+    this.objMascota.sexo= this.mascotaModal.sexo,
+    this.objMascota.imagen= this.mascotaModal.imagen,
+    this.objMascota.estado=estado;
+    this.mascotaModal.estado=estado;
+  }
+
+  getMascotaR(id: string):void{
+    this.mascotaService.getMascota(id).subscribe(
+      res=>{
+        this.mascotaModal=res;
+        this.cambiarEstadoM(1);
+        this.updateMascota();
+        Swal.fire({
+          icon: 'success',
+          title: 'Solicitud Rechazada',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      },
+      err => {throw err}
+    )
+  }
+
+  getMascotaA(id: string):void{
+    this.mascotaService.getMascota(id).subscribe(
+      res=>{
+        this.mascotaModal=res;
+        this.cambiarEstadoM(3);
+        this.updateMascota();
+        Swal.fire({
+          icon: 'success',
+          title: 'Solicitud Aceptada',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      },
+      err => {throw err}
+    )
+  }
+
+
+  updateMascota():void{
+    this.mascotaService.updateMascota(this.objMascota).subscribe(
+      res=>{
+        console.log("Se editara: ",res);
+      },
+      err =>console.error(err)
+    )
+    console.log(this.objMascota);
+  }
 }
